cac317164348cdcaf7a652d5715544c80fe50d26
Supreet <suprith.prakash.sp@gmail.com>
2021-05-27 18:59:01 +0530

    component created

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   title= "this is my test header";
   constructor() {
-  
+
    }
 
   ngOnInit(): void {
